bd835a691af56a91adc0480647f9116d241254dd
Camila Pinzon <camilapinz18@gmail.com>
2022-11-07 13:23:45 -0500

    3.2: Phonebook backend step2

--- part3/phonebook backend/index.js
+++ part3/phonebook backend/index.js
@@ -1,33 +1,50 @@
-const express=require('express')
-const app=express()
+const express = require('express')
+const app = express()
 
-const persons=
-[
-    { 
+let persons =
+  [
+    {
       "id": 1,
-      "name": "Arto Hellas", 
+      "name": "Arto Hellas",
       "number": "040-123456"
     },
-    { 
+    {
       "id": 2,
-      "name": "Ada Lovelace", 
+      "name": "Ada Lovelace",
       "number": "39-44-5323523"
     },
-    { 
+    {
       "id": 3,
-      "name": "Dan Abramov", 
+      "name": "Dan Abramov",
       "number": "12-43-234345"
     },
-    { 
+    {
       "id": 4,
-      "name": "Mary Poppendieck", 
+      "name": "Mary Poppendieck",
       "number": "39-23-6423122"
+    },
+    {
+      "id": 5,
+      "name": "Henry Olkkonen",
+      "number": "39-25-3546987"
     }
-]
+  ]
 
-app.get('/api/persons',(request,response)=>{
-    response.json(persons)
+app.get('/info', (request, response) => {
+  const info = persons.length
+  const date = new Date()
+  console.log(info)
+  response.send(
+    `<div>
+      <p>Phonebook has info for ${info} people</p>
+      <p>${date}</p>
+    </div>`)
+
 })
 
-const PORT=3001
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+const PORT = 3001
 app.listen(PORT)
