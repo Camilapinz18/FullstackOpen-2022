f90ced9d838e03faf8a62f79d5456b210f6aebf6
Camila Pinzon <camilapinz18@gmail.com>
2022-11-28 16:11:40 -0500

    eslint added

--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -1,13 +1,13 @@
 const app=require('./App')
 const http = require('http')
 const config=require('./utils/config')
-const logger=require('./utils/logger')
+
 
 const server=http.createServer(app)
 
 
 server.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`)
+    console.log(`Server running on port ${config.PORT}`)
 })
 
 
